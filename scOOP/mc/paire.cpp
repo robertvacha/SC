cfe12092c98bcbfd481d2b73944d8cf13635dcfd
Lukas Sukenik <sukeniklukas@gmail.com>
2016-04-12 15:38:59 +0200

    New Pair E calculator stump

--- scOOP/mc/paire.cpp
+++ scOOP/mc/paire.cpp
@@ -1,5 +1,56 @@
 #include "paire.h"
 
-PairE::PairE()
-{
+
+
+
+void PairE::initIntFCE() {
+    // NB
+    // Fill in the names of the functions for calculating the
+    // interaction energy
+    long geotype, other_geotype;
+    for(int i = 0; i < MAXT; i++){
+        for(int j = 0; j < MAXT; j++){
+            /* Initialize them as not existing */
+            intFCE[i][j] = &PairE::eNoExist;
+            /*geotype = topo.ia_params[i][j].geotype[0];
+            other_geotype = topo.ia_params[i][j].geotype[1];
+
+            if ( ( (geotype == CHCPSC || geotype == CPSC || geotype == TCHCPSC || geotype == TCPSC) &&
+                    (other_geotype == CHPSC || other_geotype == PSC || other_geotype == TCHPSC || other_geotype == TPSC) ) ||
+                  ( (geotype == CHPSC || geotype == PSC || geotype == TCHPSC || geotype == TPSC)  &&
+                    (other_geotype == CHCPSC || other_geotype == CPSC || other_geotype == TCHCPSC || other_geotype == TCPSC) ) )  {
+                intFCE[i][j] = &PairE::ePscPsc;
+            }
+            if ( (geotype == CHCPSC || geotype == CPSC || geotype == TCHCPSC || geotype == TCPSC) &&
+                    (other_geotype == CHCPSC || other_geotype == CPSC || other_geotype == TCHCPSC || other_geotype == TCPSC) ){
+                intFCE[i][j] = &PairE::eCpscCpsc;
+            }
+            if ( (geotype == CHPSC || geotype == PSC || geotype == TCHPSC || geotype == TPSC) &&
+                    (other_geotype == CHPSC || other_geotype == PSC || other_geotype == TCHPSC || other_geotype == TPSC) ){
+                intFCE[i][j] = &PairE::ePscPsc;
+            }
+
+            if(geotype == SCN || geotype == SPN
+                    || other_geotype == SCN || other_geotype == SPN){
+                intFCE[i][j] = &PairE::eSpnOrScn;
+            }
+            if((geotype == SCA && other_geotype == SCA)
+                    || (geotype == SPA && other_geotype == SPA)){
+                intFCE[i][j] = &PairE::e2ScaOr2Spa;
+            }
+            if((geotype == SCA && other_geotype == SPA)
+                    || (geotype == SPA && other_geotype == SCA)){
+                intFCE[i][j] = &PairE::eSpaSca;
+            }
+            if(( (geotype == PSC || geotype == CHPSC || geotype == TCHPSC || geotype == TPSC) && other_geotype == SPA)
+                    || (geotype == SPA && (other_geotype == PSC||other_geotype == CHPSC || other_geotype == TCHPSC || other_geotype == TPSC) )){
+                intFCE[i][j] = &PairE::ePscSpa;
+            }
+            if(( (geotype == CPSC ||geotype == CHCPSC || geotype == TCHCPSC || geotype == TCPSC) && other_geotype == SPA)
+                    || (geotype == SPA && (other_geotype == CPSC||other_geotype == CHCPSC || other_geotype == TCHCPSC || other_geotype == TCPSC)  )){
+                intFCE[i][j] = &PairE::eCpscSpa;
+            }*/
+
+        }
+    }
 }
--- scOOP/mc/paire.h
+++ scOOP/mc/paire.h
@@ -1,10 +1,53 @@
+/** @file paire.h*/
+
 #ifndef PAIRE_H
 #define PAIRE_H
 
-class PairE
-{
+#include "../structures/topo.h"
+#include "../structures/Conf.h"
+
+class PairE {
+private:
+    Particle* part1;
+    Particle* part2;
+
+    ConList* conlist;
+
+    /*
+     *  Parameters of interaction calculation
+     */
+    double dist;                    // closest distance
+    Vector distvec;                 // vector of closes distance
+    Vector r_cm;                    // Vector between centres of mass from part2 to part1
+    double distcm;                  // distance between center of masses
+    double dotrcm;                  // square size of r_cm
+    double contt;                   // closest point on spherocylinder to sphere
+
+    GeoBase* pbc;                   // box size
+
+    double (PairE::*intFCE[MAXT][MAXT])();
+
+
 public:
-    PairE();
+    PairE(GeoBase* pbc) : pbc(pbc) {}
+
+    double operator() (Particle* part1, Particle* part2, ConList* conlist=NULL);
+
+    /**
+     * @brief init_intfce Initializes the array with the pointers to the energy function
+     */
+    void initIntFCE();
+
+private:
+
+    /**
+     * @brief enoexist    Indicates not yet programmed interaction
+     * @return
+     */
+    double eNoExist() {
+        fprintf (stderr, "ERROR: We have not programed interaction of types %d and %d\n", part1->type,part2->type);
+        return 0.0;
+    }
 };
 
 #endif // PAIRE_H
